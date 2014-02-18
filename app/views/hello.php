b461e79306247cab0570d1aff392ba097b0548d8
hesperus1usg <hesperus1usg@gmail.com>
2014-02-18 20:33:55 +0700

    init commit
    init commit

---
+++ app/views/hello.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta http-equiv="content-type" content="text/html" charset="utf-8"/>
+	<meta name="author" content="lolkittens" />
+
+	<title>Untitled 1</title>
+    <style type="text/css">
+    *{padding:0px; margin:0px;}
+    #wrapper
+    {
+        width:100%;
+        height:100%;
+        margin:auto;
+    }
+    h1#logo
+    {
+        text-align: center;
+        margin-top:100px;
+    }
+    #button
+    {
+        width:100%;
+        height:50px;
+        position: relative;
+        text-align: center;
+        padding:27px 0;
+        
+    }
+    #button a
+    {
+        background-color:#4060a0;
+    }
+    
+    </style>
+</head>
+
+<body>
+<div id="wrapper">
+<h1 id="logo"><img src="http://localhost/laravel/public/logo-jobforward.png"/> </h1>
+<div id="button">
+<a href="#"><img src="http://localhost/laravel/public/Login.png" /></a>
+</div>
+<div style="clear: both;"></div>
+</div>
+</body>
+</html>
